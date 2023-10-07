27c29869358740089c902dc14025ebe76c3244c1
Travis <tdcreamer@outlook.com>
2023-10-07 17:36:45 -0400

    Added init logic and fixed button press function

--- AE_Control.ino
+++ AE_Control.ino
@@ -1,18 +1,24 @@
 
-const int buttonPin = 1;//x;
-const int actuatorPin = 2;//y;
+#include <TimerOne.h>
 
-const int led1Pin = 3;//a;
-const int led2Pin = 4;//b;
-const int buttonPressTime = 500; //In Milliseconds
+const int buttonPin = 2;
+const int actuatorPin = 9;            //Pin connected to the exhaust actuator circuit. Timer1 can override Pin 9 and Pin 10.
+
+const int led1Pin = 3;
+const int led2Pin = 4;
+const int buttonPressTime = 500;      //In Milliseconds
 
 int actuatorState = 0;
 int maxActuatorState = 2;
 
-long buttonUpTime = 0;
-long buttonDownTime = 0;
+unsigned long buttonUpTime = 0;
+unsigned long buttonDownTime = 0;
 
 int buttonVal;
+
+float frequency = 100;                // Frequency in Hertz
+float dutyCycle;                      // Duty cycle (pulse width) percentage
+float period = 1000000 / frequency;   //1 Second in microseconds divided by the frequency
 
 
 void setup() {
@@ -24,6 +30,12 @@ void setup() {
   pinMode(led1Pin, OUTPUT);
   pinMode(led2Pin, OUTPUT);
 
+  initializeActuator();
+
+  Timer1.initialize(period);
+  Timer1.start();
+  Timer1.pwm(actuatorPin, 0);
+
 }
 
 void loop() {
@@ -31,22 +43,31 @@ void loop() {
   bool check = buttonPressed();
 
   updateState(check);
-  
-  Serial.print("Button: " + check + " State: " + actuatorState);
 
+ Serial.println(actuatorState);
 
 }
 
 bool buttonPressed() {
 
+  // Read button status
   buttonVal = digitalRead(buttonPin);
-  
-  if(buttonVal == 0)
+
+  // If button is not pressed, check if the time difference was reached indicating a valid press and set the current time. 
+  if(buttonVal == LOW) {
+    if(((long)(buttonDownTime - buttonUpTime)) > 150)
+    {
+      buttonUpTime = millis();
+      return true;
+    }
+      
     buttonUpTime = millis();
-  else if(buttonVal == 1)
+
+  }
+  else if(buttonVal == HIGH)
     buttonDownTime = millis();
 
-  return (buttonDownTime - buttonUpTime) > 500;
+  return false;
   
 }
 
@@ -56,6 +77,26 @@ void updateState(bool next){
     actuatorState++;
 
   if(actuatorState > maxActuatorState)
-    acuatorState = 0;
+    actuatorState = 0;
+
+}
+
+void initializeActuator(){
+  //Cycles the signal sent to the actuator to allow it to self calibrate and be ready
+  
+  digitalWrite(ledPin1, HIGH);
+  digitalWrite(ledPin2, HIGH);
+  
+  digitalWrite(actuatorPin, LOW);
+  delay(250);
+
+  digitalWrite(actuatorPin, HIGH);
+  delay(1000);
+
+  digitalWrite(acutatorPin, LOW);
+  delay(1000);
+
+  digitalWrite(ledPin1, LOW);
+  digitalWrite(ledPin2, LOW);
 
 }
